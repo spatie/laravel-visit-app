fb3318f3b58ac06a0d629f9884003c394be85189
Freek Van der Herten <freek@spatie.be>
2022-02-20 19:17:42 +0100

    wip

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::get('/json', JsonController::class);
+
+Route::get('/exception', function() {
+   throw new Exception('This is my exception');
+});
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -13,6 +13,7 @@ class ExampleTest extends TestCase
      */
     public function test_that_true_is_true()
     {
+
         $this->assertTrue(true);
     }
 }
